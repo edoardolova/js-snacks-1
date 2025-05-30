534cbfea16aa8b9fd5d6b95d5240f8d7ea2dbeb0
Edoardo Lova <edoardolova81@gmail.com>
2025-05-30 11:30:28 +0200

    add snack 2

---
+++ snack2/main.js
@@ -0,0 +1,13 @@
+let input1= prompt("inserisci la prima parola");
+let input2= prompt("inserisci la seconda parola");
+
+if (input1.length > input2.length) {
+    console.log(`la parola più lunga è ${input1}`);
+}
+else if (input2.length > input1.length) {
+    console.log(`la parola più lunga è ${input2}`);
+}
+else{
+    console.log(`le due parole sono lunghe uguali`);
+
+}
