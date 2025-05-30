dc0cc9d193d3796b4b36954dd67e70de403006be
Edoardo Lova <edoardolova81@gmail.com>
2025-05-30 11:44:32 +0200

    fix log both word

--- snack2/main.js
+++ snack2/main.js
@@ -2,9 +2,11 @@ let input1= prompt("inserisci la prima parola");
 let input2= prompt("inserisci la seconda parola");
 
 if (input1.length > input2.length) {
+    console.log(`la parola più corta è ${input2}`);
     console.log(`la parola più lunga è ${input1}`);
 }
 else if (input2.length > input1.length) {
+    console.log(`la parola più corta è ${input1}`);
     console.log(`la parola più lunga è ${input2}`);
 }
 else{
